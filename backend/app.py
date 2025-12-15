89cfa7d51e58c3bb507dca25dd58b65231da426c
Константин Макаренко <kostamak@MacBook-Pro-Konstantin-2.local>
2025-12-15 15:46:54 +0300

    Написан get заглушка

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
+from database import Base, engine, get_db
+from models import Garden
+
+# Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Garden")
 
 
+@app.get("/")
+def main():
+    return {"Hello": "World"}
+@app.get("/gardens")
+def get_gardens(db: Session = Depends(get_db)):
+    return db.query(Garden).all()
